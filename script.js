fbe01550f6461d01c47013506516eb6a7129a4cd
Mattia <contactperso.mp@gmail.com>
2024-03-17 13:02:31 +0100

    Ajouté des liens dans le menu et corrigé les prix des hôtels

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-var burgerMenu = document.getElementById("burger-menu");
-var overlay = document.getElementById("menu");
+let burgerMenu = document.getElementById("burger-menu");
+let overlay = document.getElementById("menu");
+
+let bugerEl = document.querySelectorAll(".burgerEl");
 burgerMenu.addEventListener("click", function () {
   this.classList.toggle("close");
   overlay.classList.toggle("overlay");
   document.body.classList.toggle("cs");
+});
+
+bugerEl.forEach((element) => {
+  element.addEventListener("click", function () {
+    burgerMenu.classList.remove("close");
+    overlay.classList.remove("overlay");
+    document.body.classList.remove("cs");
+  });
 });
