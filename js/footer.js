38db2c1de072f2d0b60df23723e754ddbab93294
Mattia PARRINELLO <contactperso.mp@gmail.com>
2024-03-18 10:20:05 +0100

    Regrouper les codes répétitifs dans des fichiers js afin de gagner de la place

---
+++ js/footer.js
@@ -0,0 +1,3 @@
+document.write(`<footer>
+<p>&copy; Mattia PARRINELLO - 2024 - All Rights Reserved</p>
+</footer>`)
---
+++ js/header.js
@@ -0,0 +1,21 @@
+document.write(`        <header class="navbar">
+<h3><a href="/#pres">Présentation</a></h3>
+<h3><a href="/#hotels">Hôtel</a></h3>
+<h3><a href="/#incontournables">Les incontournables</a></h3>
+<h3><a href="/enSavoirPlus.html">En savoir plus</a></h3>
+</header>
+<div id="burger-menu">
+<span></span>
+</div>
+<header id="menu">
+<ul>
+  <li><a class="burgerEl" href="/#pres">Présentation</a></li>
+  <li><a class="burgerEl" href="/#hotels">Hotel</a></li>
+  <li>
+    <a class="burgerEl" href="/#incontournables"
+      >Les incontournables</a
+    >
+  </li>
+  <li><a class="burgerEl" href="/enSavoirPlus.html">En savoir plus</a></li>
+</ul>
+</header>`)
