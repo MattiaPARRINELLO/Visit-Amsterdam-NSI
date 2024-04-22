e08bb0288e166fb3dd7009ffd56b1116c1fb03e7
Mattia PARRINELLO <contactperso.mp@gmail.com>
2024-04-22 09:26:29 +0200

    Corrected path error and added some easterEgg

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 let burgerMenu = document.getElementById("burger-menu");
 let overlay = document.getElementById("menu");
 
+let bigTittle = document.getElementById("BT")
+let smallTittle = document.getElementById("ST")
+let clickBT = 0
+let clickST = 0
+console.log("init")
 let bugerEl = document.querySelectorAll(".burgerEl");
 burgerMenu.addEventListener("click", function () {
   this.classList.toggle("close");
@@ -17,9 +22,29 @@ bugerEl.forEach((element) => {
 });
 
 
+const secretBT = () => {
+  clickBT++
+  if (clickBT >= 2) {
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank");
+    clickBT = 0
+  }
+}
+const secretST = () => {
+  clickST++
+  if (clickST >= 2) {
+    window.open("https://mattiaparrinello.github.io/Super-Morpion/", "_blank");
+    clickST = 0
+  }
+
+}
+
+bigTittle.addEventListener("click", secretBT)
+smallTittle.addEventListener("click", secretST)
+
+
 //Je travail chez Google donc :
 const cookies = () => {
-  if (user.cookies.accepted) {
+  if (user.cookies.aàccepted) {
     collectUserData()
   }
   else {
