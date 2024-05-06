f274473ec3d02bb5ff916fed66a0750b5ac863de
Mattia <contactperso.mp@gmail.com>
2024-05-06 14:13:06 +0200

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,21 +1,21 @@
 document.write(`        <header class="navbar">
-<h3><a href="HTML/#pres">Présentation</a></h3>
-<h3><a href="HTML/#hotels">Hôtel</a></h3>
-<h3><a href="HTML/#incontournables">Les incontournables</a></h3>
-<h3><a href="HTML/enSavoirPlus.html">En savoir plus</a></h3>
+<h3><a href="#pres">Présentation</a></h3>
+<h3><a href="#hotels">Hôtel</a></h3>
+<h3><a href="#incontournables">Les incontournables</a></h3>
+<h3><a href="enSavoirPlus.html">En savoir plus</a></h3>
 </header>
 <div id="burger-menu">
 <span></span>
 </div>
 <header id="menu">
 <ul>
-  <li><a class="burgerEl" href="HTML/#pres">Présentation</a></li>
-  <li><a class="burgerEl" href="HTML/#hotels">Hotel</a></li>
+  <li><a class="burgerEl" href="#pres">Présentation</a></li>
+  <li><a class="burgerEl" href="#hotels">Hotel</a></li>
   <li>
-    <a class="burgerEl" href="HTML/#incontournables"
+    <a class="burgerEl" href="#incontournables"
       >Les incontournables</a
     >
   </li>
-  <li><a class="burgerEl" href="HTML/enSavoirPlus.html">En savoir plus</a></li>
+  <li><a class="burgerEl" href="enSavoirPlus.html">En savoir plus</a></li>
 </ul>
 </header>`)
